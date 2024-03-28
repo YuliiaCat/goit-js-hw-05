47bdb49a05fa69ab3633645751216bf4655a22d1
Yuliia <kotyuliya24@gmail.com>
2024-03-28 18:19:37 -0400

    task2

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const getUsersWithFriend = (users, friendName) => {
-  return users.filter(user => user.friends === friendName && user.friends.includes(friendName));
+  return users.filter(user => user.friends === friendName && friends.includes(friendName));
 }
 
 const allUsers = [
